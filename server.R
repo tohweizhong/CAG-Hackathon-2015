f6b5d95d94e7bd874fd2bfbf367036e50294deb2
tohweizhong <tohweizhong@hotmail.com>
2015-10-04 17:44:29 +0800

    041015 1745 @ cleaned up codes

--- server.R
+++ server.R
@@ -1,5 +1,3 @@
-# TODO
-
 # set-up
 source("codes/houseblend.R")
 
@@ -16,16 +14,6 @@ shinyServer(function(session, input, output){
         return(profile)
     })
     
-#     Terminal <- reactive({
-#         if(input$"_flightNum" != ""){
-#             flightNum <- input$"_flightNum"
-#             idx <- which(flights.df$Flight.No == flightNum)
-#             terminal <- flights.df[idx]
-#             return(terminal)
-#         }
-#         else return("")
-#     })
-    
     Filename.png <- reactive({
         root <- input$"_root"
         # either one, or neither
@@ -33,21 +21,7 @@ shinyServer(function(session, input, output){
         profile <- gsub(x = profile, pattern = "<", replacement = "less")
         profile <- gsub(x = profile, pattern = ">", replacement = "more")
         
-#         if(input$"_flightNum" != ""){
-#             
-#             # extract terminal
-#             flightNum <- input$"_flightNum"
-#             idx <- which(flights.df$Flight.No == flightNum)
-#             terminal <- flights.df$Terminal[idx]
-#             filename <- paste("images/network images/NEW network images/", terminal, " images/", root, "_", profile, ".png", sep = "")
-#         }
-#         else{
-#             filename <- paste("images/network images/", root, "_", profile, ".png", sep = "") 
-#         }
-#         print(filename)
-        
         filename <- paste("images/network images/", root, "_", profile, ".png", sep = "")
-        print(filename)
         return(filename)
     })
     
@@ -68,40 +42,6 @@ shinyServer(function(session, input, output){
     })
     
     output$showFlightNum <- renderText({input$"_flightNum"})
-
-    
-#     Profile2Facilities <- reactive({
-#         
-#         # extract the selected category
-#         this.cate <- params$whichCate
-#         if(this.cate == "Rest & Relax") this.cate <- "R&R"
-#         else if(this.cate == "Food & Beverages") this.cate <- "F&B"
-#         
-#         # extract the profile
-#         this.profile <- paste(input$"_natl", ", ",
-#                          input$"_gender", ", ",
-#                          input$"_age", sep = "")
-#         
-#         # extract the corresponding indices
-#         this.idx <- which(rules$Profile == this.profile)
-#         
-#         # extract the corresponding popular facilities
-#         this.facilities <- union(rules$From[idx], rules$To[idx])
-#         this.facilities <- sort(this.facilities)
-#         
-#         # match the facilities with categories
-#         this.facilities.cate <- unlist(sapply(this.facilities, MatchFacWithCate))
-#         
-#         # extract the facilities in the category
-#         this.idx2 <- which(this.facilities.cate == this.cate)
-#         this.facilities <- this.facilities[this.idx2]
-#         
-#         returnMe <- data.frame(cbind(this.facilities, this.facilities.cate),
-#                                stringsAsFactors = F)
-#         colnames(returnMe) <- c("Facility", "Category")
-#         
-#         return(returnMe)
-#     })
     
     # ==== 2. Select which broad category ==== #
     params <- reactiveValues(whichCate = " ", submitted = FALSE)
@@ -150,98 +90,35 @@ shinyServer(function(session, input, output){
         # extract the facilities in the category
         idx2 <- which(.choices.cate == cate)
         .choices <- .choices[idx2]
-        print(.choices)
-        
-        # extract facilities from the correct terminal
-#         if(input$"_flightNum" != ""){
-#             
-#             # extract terminal
-#             flightNum <- input$"_flightNum"
-#             idx <- which(flights.df$Flight.No == flightNum)
-#             terminal <- flights.df$Terminal[idx]
-#             print(terminal)
-#             # subset facilities based on terminal
-#             idx <- which(cag.directory$Location == terminal)
-#             print(idx)
-#             .choices <- intersect(.choices, cag.directory$ShopName[idx])
-#             print(.choices)
-#         }
-#         
-#         if(params$whichCate == " ")
-#             .choices <- ""
-#         if(params$whichCate == "Rest & Relax"){
-#             tmp.df <- Profile2Facilities()
-#             idx <- which(tmp.df$Category == "Rest & Relax")
-#             .choices <- tmp.df$Facility[idx]
-#             
-#         }
-#         else if(params$whichCate == "Food & Beverages")
-#             .choices <- c("1")
-#         else if(params$whichCate == "Shopping")
-#             .choices <- c("2")
-        
         .choices <- as.pairlist(.choices)
         
         updateSelectInput(session, "_root", choices = .choices)
-        
         params$submitted <- FALSE
     })
     
     # ==== 4. Display Markov recommendations and other information ==== #
-    # this is where the rendering images with if else
     output$showMarkov <- renderImage({
-        
         if(params$submitted == FALSE)
             list(src = "images/logo3.png", alt = NULL, width = 600)
         
         else if(params$whichCate == "None")
             list(src = "images/logo3.png", alt = NULL, width = 600)
         
-        ###
-        
-        else
-            
-            list(src = Filename.png(), alt = NULL, width = 600)
-        
-#         else if(params$whichCate == "Rest & Relax")
-#             list(src = "images/TWG Tea Boutique_Chinese, F, more64yrs.png", alt = NULL, width = 800)
-#         
-#         else if(params$whichCate == "Shopping" && input$"_root" == "Guardian")
-#             list(src = "images/example_guardian.png", alt = NULL, width = 800)
-#         
-#         else if(params$whichCate == "Shopping" && input$"_root" == "Longchamp")
-#             list(src = "images/example_longchamp.png", alt = NULL, width = 800)
-#         
-#         else if(params$whichCate == "Food & Beverages" && input$"_root" == "Ippudo Express")
-#             list(src = "images/example_longchamp.png", alt = NULL, width = 800)
+        else list(src = Filename.png(), alt = NULL, width = 600)
     }, deleteFile = F)
-    
     
     # attempt to render plot
     output$showMarkov2 <- renderPlot({
         goPlot(CHOSEN = input$"_root", pfl = Profile())
     })
     
-    
-#     output$showInfo <- renderDataTable({
-#         if(params$submitted == TRUE){
-#             df <- data.frame(matrix(99, nrow = 2, ncol = 2))
-#             colnames(df) <- c("Facility", "Location")
-#             return(df)
-#         }
-#     }, options = list(searching = FALSE, paging = FALSE))
-    
     output$showTime <- renderText({
-        
         flightNum <- input$"_flightNum"
         
         if(flightNum != ""){
             idx <- which(flights.df$Flight.No == flightNum)
             flightTime <- flights.df$Time[idx]
-            
-            #flightDate <- flights$Date
-            #datetime <- paste()
-            
+
             timeDiff <- strptime(flightTime, "%H%M") - Sys.time()
             idx2 <- gregexpr("[0-9,.]+", timeDiff)
             timeDiff <- as.numeric(unique(unlist(regmatches(timeDiff, idx2))))
@@ -250,12 +127,10 @@ shinyServer(function(session, input, output){
             mins <- round((timeDiff %% 1) * 60)
             hrs <- timeDiff - timeDiff %% 1
             
-            #returnMe <- paste("Time to flight: ", hrs, "hrs and ", mins, " mins.", sep = "")
             returnMe <- paste(hrs, " hrs and ", mins, " mins", sep = "")
             return(returnMe) 
         }
         return("")
-           
     })
     
     output$showLegend0 <- renderText({
@@ -266,37 +141,31 @@ shinyServer(function(session, input, output){
     
     output$showLegend1 <- renderText({
         if(params$submitted == TRUE){
-            legend <- "Purple: Rest & Relax"
-            return(legend)
+            return("Purple: Rest & Relax")
         }
     })
     
     output$showLegend2 <- renderText({
         if(params$submitted == TRUE){
-            legend <- "Turquoise: Food & Beverages"
-            return(legend)
+            return("Turquoise: Food & Beverages")
         }
-        
     })
     
     output$showLegend3 <- renderText({
         if(params$submitted == TRUE){
-            legend <- "Orange: Shopping"
-            return(legend)
+            return("Orange: Shopping")
         }
     })
     
     output$showLegend4 <- renderText({
         if(params$submitted == TRUE){
-            legend <- "Size of shape: average time spent at shop"
-            return(legend)
+            return("Size of shape: average time spent at shop")
         }
     })
     
     output$showLegend5 <- renderText({
         if(params$submitted == TRUE){
-            legend <- "Thickness of arrow: popularity of shop"
-            return(legend)
+            return("Thickness of arrow: popularity of shop")
         }
     })
 })
--- ui.R
+++ ui.R
@@ -7,92 +7,61 @@ shinyUI(fluidPage(
     
     tags$hr(),
     
-    #fluidRow(
-     sidebarLayout(
-         sidebarPanel(
-        # ==== 1. capture demographics ==== #
-        #column(2,
-               tags$br(),
-               uiOutput("selectNatl"),
-               uiOutput("selectAge"),
-               uiOutput("selectGender"),
-        #),
+    sidebarLayout(
         
-        # ==== 2. capture flight number ==== #
-        #column(2,
-               tags$br(),
-               textInput("_flightNum", label = "Enter your flight number:", value = ""),
-               tags$h5("Time to flight:"),
-               #verbatimTextOutput("showFlightNum"),
-               verbatimTextOutput("showTime"),
-        #),
-        
-        # ==== 3. Select which broad category ==== #
-        #column(4,
-               tags$h5("What would you like to do first?"),
-               actionButton("_r&r", "Rest & Relax", icon = icon("pause", class = "fa-spin", lib = "font-awesome")),
-               actionButton("_f&b", "Food & Beverages", icon = icon("cutlery", class = "fa-spin", lib = "font-awesome")),
-               actionButton("_shopping", "Shopping", icon = icon("shopping-cart", class = "fa-spin", lib = "font-awesome")),
-               tags$h5("You have selected:"),
-               verbatimTextOutput("showWhichCate"),
-        #),
-        
-        # ==== 4. Select root node ==== #
-        #column(3,
-               tags$br(),
-               selectInput("_root", "Where would you like to go first?", choices = ""),
-               actionButton("_submit", "What does Genie recommend?", icon = icon("plane", lib = "font-awesome")),
-               tags$br(),
-        tags$br(),
-        actionButton("_like", "Do you like Genie's recommendation?", icon = icon("thumbs-o-up", lib = "font-awesome")),
-               tags$h6("© XGB-Protocol for CAG Hackathon 2015")
-        #)
-    ),
-    
-    mainPanel(
-        
-        
-        
-        
-   
-    
-    # ==== 5. Display Markov recommendations and required information ==== #
-    #fluidRow(
-        #column(8,
-        fluidRow(
-               plotOutput("showMarkov"),
-               #),
-        #column(3,
-               textOutput("showLegend0"),
-               tags$head(tags$style(" #showLegend0{font-size: 24px;
-                                    font-style: bold;}")),
-               tags$br(),
-               textOutput("showLegend1"),
-               tags$head(tags$style("#showLegend1{color: #A16CC1;
-                                 font-size: 16px;
-                                 font-style: bold;
-                                 }")),
-               textOutput("showLegend2"),
-               tags$head(tags$style("#showLegend2{color: #7FFFD4;
-                                 font-size: 16px;
-                                    font-style: bold;
-                                    }")),
-               textOutput("showLegend3"),
-               tags$head(tags$style("#showLegend3{color: #F0E68C;
-                                 font-size: 16px;
-                                 font-style: bold;
-                                 }"))
+        sidebarPanel(
+            # ==== 1. capture demographics ==== #
+            tags$br(),
+            uiOutput("selectNatl"),
+            uiOutput("selectAge"),
+            uiOutput("selectGender"),
+            
+            # ==== 2. capture flight number ==== #
+            tags$br(),
+            textInput("_flightNum", label = "Enter your flight number:", value = ""),
+            tags$h5("Time to flight:"),
+            #verbatimTextOutput("showFlightNum"),
+            verbatimTextOutput("showTime"),
+            
+            # ==== 3. Select which broad category ==== #
+            tags$h5("What would you like to do first?"),
+            actionButton("_r&r", "Rest & Relax", icon = icon("pause", class = "fa-spin", lib = "font-awesome")),
+            actionButton("_f&b", "Food & Beverages", icon = icon("cutlery", class = "fa-spin", lib = "font-awesome")),
+            actionButton("_shopping", "Shopping", icon = icon("shopping-cart", class = "fa-spin", lib = "font-awesome")),
+            tags$h5("You have selected:"),
+            verbatimTextOutput("showWhichCate"),
+            
+            # ==== 4. Select root node ==== #
+            tags$br(),
+            selectInput("_root", "Where would you like to go first?", choices = ""),
+            actionButton("_submit", "What does Genie recommend?", icon = icon("plane", lib = "font-awesome")),
+            tags$br(),
+            tags$br(),
+            actionButton("_like", "Do you like Genie's recommendation?", icon = icon("thumbs-o-up", lib = "font-awesome")),
+            tags$h6("© XGB-Protocol for CAG Hackathon 2015")
         ),
-        tags$br(),
-        fluidRow(
-               textOutput("showLegend4"),
-               tags$head(tags$style(" #showLegend4{font-size: 16px;
-                                    font-style: bold;}")),
-               textOutput("showLegend5"),
-               tags$head(tags$style(" #showLegend5{font-size: 16px;
-                                    font-style: bold;}"))
-               
-        )
+        
+        mainPanel(
+            # ==== 5. Display Markov recommendations and required information ==== #
+            fluidRow(
+                plotOutput("showMarkov"),
+                textOutput("showLegend0"),
+                tags$head(tags$style("#showLegend0{font-size: 24px; font-style: bold;}")),
+                tags$br(),
+                textOutput("showLegend1"),
+                tags$head(tags$style("#showLegend1{color: #A16CC1; font-size: 16px; font-style: bold;}")),
+                textOutput("showLegend2"),
+                tags$head(tags$style("#showLegend2{color: #7FFFD4; font-size: 16px; font-style: bold;}")),
+                textOutput("showLegend3"),
+                tags$head(tags$style("#showLegend3{color: #F0E68C; font-size: 16px; font-style: bold;}"))
+            ),
+            tags$br(),
+            fluidRow(
+                textOutput("showLegend4"),
+                tags$head(tags$style(" #showLegend4{font-size: 16px; font-style: bold;}")),
+                textOutput("showLegend5"),
+                tags$head(tags$style(" #showLegend5{font-size: 16px; font-style: bold;}"))
+            )
         )
     )
 ))
